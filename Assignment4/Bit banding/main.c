dc44ffa812f3457c00f02bdb2c0d619fc49434fe
Shruti Awale <shrutia@uw.edu>
2020-11-27 14:59:04 -0800

    assignment 4

---
+++ Assignment4/Bit banding/main.c
@@ -0,0 +1,37 @@
+#if 1
+
+#define RCC_BASE 0x40021000
+#define GPIOA_BASE 0x48000000
+
+#define RCC_AHB2ENR (*((unsigned int*)(RCC_BASE + 0x4C)))
+#define GPIOA_MODER (*((unsigned int*)(GPIOA_BASE + 0x0)))
+#define GPIOA_ODR (*((unsigned int*)(GPIOA_BASE + 0x14)))
+#define ORD5 (1<<5)
+
+volatile int counter =0;
+int main()
+{
+    RCC_AHB2ENR |= 0x1;
+    
+    *((unsigned int*)((0x42000000) +(0x2104c * 32)+ (0 *4))) = 0x1;
+    GPIOA_MODER &= 0xFFFFF7FF;
+    int counter = 0;
+  
+  while(1)
+  {
+      counter =0;
+     while(counter < 100000) 
+     {
+         counter ++;
+     }
+     GPIOA_ODR^= ORD5;
+       counter =0;
+   while(counter < 100000) 
+     {
+         counter ++;
+     }
+          GPIOA_ODR^= ORD5;
+}
+}
+#endif
+
