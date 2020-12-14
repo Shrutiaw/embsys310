5e0235f8a88b2a17726d713ee492b1ab09fb0e22
Shruti Awale <shrutia@uw.edu>
2020-12-14 13:11:06 -0800

    main

--- Assignment6/CMSIS LED1/main.c
+++ Assignment6/CMSIS LED1/main.c
@@ -40,17 +40,17 @@ int main()
     RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
     
     // GPIO port mode register (GPIOx_MODER) (x = A..E and H)
-    // GPIOB Base Address: 0x48000400
+    // GPIOA Base Address: 0x48000400
     // Address offset: 0x00
     // Set bit[29:28] to 0x01  so --> 0x400 // To enable PB14 as output
-    GPIOB->MODER &= ~GPIO_MODER_MODE5_1;
-    GPIOB->MODER |= GPIO_MODER_MODE5_0;
+    GPIOA->MODER &= ~GPIO_MODER_MODE5_1;
+    GPIOA->MODER |= GPIO_MODER_MODE5_0;
     
     // GPIO port output data register (GPIOx_ODR) (x = A..E and H)
-    // GPIOB Base Address: 0x48000400
+    // GPIOA Base Address: 0x48000400
     // Address offset: 0x14
-    // Set bit[14] to 1 --> 0x4000; // Turn LED ON
-    // Set bit[14] to 0 --> 0x0; // Turn LED OFF
+    // Set bit[5] to 1 --> 0x4000; // Turn LED ON
+    // Set bit[5] to 0 --> 0x0; // Turn LED OFF
     
     while(1)
     {
---
+++ Assignment7/README.md
@@ -0,0 +1,17 @@
+2) Generate the map file for your program and provide details on:
+
+a) How much total ROM your program is occupying?
+
+-The program is using 432bytes of ROM.
+
+
+b. How much total RAM your program is using?
+-The program is using 8196 bytes of RAM
+
+
+c. What part of your program is using the most ROM?
+
+-The most ROM is used in delay.o i.e 52 bytes.
+
+
+d. What part of your program is using the most RAM?
